d068a6f87edd5a28fac4c0041512d6718dfb4ef0
ejkim0422 <ejkim0422@gmail.com>
2014-03-27 13:50:17 +0900

    add indicator function

---
+++ AbstractAnalyzer.java
@@ -0,0 +1,12 @@
+package com.codescroll.qscroll.revisionCreator;
+
+public abstract class AbstractAnalyzer {
+	protected int t_revisionId;
+	
+	public abstract void analyze();
+}
+
+//20140327 modify
+public void indicator(){
+
+}
